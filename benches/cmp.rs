1f632d1112db6bf92a27c5a75554796e442ec9cf
Philipp Korber <philippkorber@gmail.com>
2018-01-12 15:20:37 +0100

    chore: improved benchmarks

--- benches/cmp.rs
+++ benches/cmp.rs
@@ -3,12 +3,31 @@
 extern crate mime;
 extern crate test;
 
-use mime::{MediaType, TEXT};
+use mime::{MediaType, TEXT, PLAIN, CHARSET, AsciiCaseInsensitiveEq};
 use mime::spec::{HttpSpec, Obs};
-use test::Bencher;
+use test::{Bencher, black_box};
 
 #[bench]
-fn bench_eq_parsed_both(b: &mut Bencher) {
+fn is_charset_latin1(b: &mut Bencher) {
+    b.iter(|| {
+        let mime = MediaType::<HttpSpec>::parse(black_box("text/plain; charset=utf-8")).unwrap();
+        match (mime.type_(), mime.subtype()) {
+            (TEXT, PLAIN) => {
+                for (param, value) in mime.params() {
+                    match param {
+                        CHARSET if value.eq_ignore_ascii_case("latin1") => (),
+                        _ => return false,
+                    }
+                }
+            }
+            _ => return false
+        }
+        true
+    })
+}
+
+#[bench]
+fn bench_eq_parsed_both_utf8(b: &mut Bencher) {
     let mime = MediaType::<HttpSpec<Obs>>::parse("text/plain; charset=utf-8").unwrap();
     let pre_parsed = MediaType::<HttpSpec<Obs>>::parse("text/plain; charset=utf-8").unwrap();
     b.bytes = mime.as_str_repr().len() as u64;
@@ -18,7 +37,7 @@ fn bench_eq_parsed_both(b: &mut Bencher) {
 }
 
 #[bench]
-fn bench_eq_parsed_both_non_charset(b: &mut Bencher) {
+fn bench_eq_parsed_both_non_charset_utf8(b: &mut Bencher) {
     let mime = MediaType::<HttpSpec<Obs>>::parse("text/plain; foo=bar").unwrap();
     let pre_parsed = MediaType::<HttpSpec<Obs>>::parse("text/plain; foo=bar").unwrap();
     b.bytes = mime.as_str_repr().len() as u64;
@@ -36,7 +55,6 @@ fn bench_eq_parsed_both_non_charset(b: &mut Bencher) {
 //    });
 //}
 
-
 #[bench]
 fn bench_ne_parsed_subtype(b: &mut Bencher) {
     let left = MediaType::<HttpSpec<Obs>>::parse("text/plain; charset=utf-8").unwrap();
@@ -46,7 +64,6 @@ fn bench_ne_parsed_subtype(b: &mut Bencher) {
         assert_ne!(left, right);
     });
 }
-
 
 #[bench]
 fn bench_eq_type_(b: &mut Bencher) {
